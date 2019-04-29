855919de54ed0eb839e1d5a47c23851451ce722f
melonybox <grandmadcow@gmail.com>
2019-04-29 15:57:55 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Ride.destroy_all
+Taxi.destroy_all
+Passenger.destroy_all
